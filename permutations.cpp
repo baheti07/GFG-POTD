3aa666fa0d6cc1f9a69f13a5687c896edfeb5e3c
baheti07 <90107385+baheti07@users.noreply.github.com>
2023-08-06 15:11:49 +0530

    Add files via upload

---
+++ permutations.cpp
@@ -0,0 +1,16 @@
+vector<vector<int>> permute(vector<int>& nums) {
+       vector < vector < int >> ans;
+      recurPermute(0, nums, ans);
+      return ans;
+    }
+   void recurPermute(int index, vector < int > & nums, vector < vector < int >> & ans) {
+      if (index == nums.size()) {
+        ans.push_back(nums);
+        return;
+      }
+      for (int i = index; i < nums.size(); i++) {
+        swap(nums[index], nums[i]);
+        recurPermute(index + 1, nums, ans);
+        swap(nums[index], nums[i]);
+      }
+    }
