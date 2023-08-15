5f210b197212d2a40e91e3e0333df4f0c4c3f3ad
baheti07 <90107385+baheti07@users.noreply.github.com>
2023-08-15 15:34:16 +0530

    Add files via upload

---
+++ flipbitsgfg.cpp
@@ -0,0 +1,21 @@
+int maxOnes(int a[], int n)
+    {
+        // Your code goes here
+         int mx=0,c=0,cnt=0;
+        for(int i=0;i<n;i++)
+        {
+            if(c<0)c=0;
+            if(c>=0)
+            {
+                if(a[i]==0)
+                {
+                    c++;
+                    mx=max(mx,c);
+                }
+                else{
+                    cnt++;
+                    c--;
+                }            }
+        }
+        return (cnt+mx);
+    }
