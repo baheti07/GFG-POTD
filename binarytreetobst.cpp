ed3b887e0aa236110b45e64c626963cfcdaf2739
baheti07 <90107385+baheti07@users.noreply.github.com>
2023-09-09 18:52:14 +0530

    Add files via upload

---
+++ binarytreetobst.cpp
@@ -0,0 +1,36 @@
+ int idx;
+    
+    Node* binaryTreeToBST(Node* root) {
+        if (root == nullptr)
+            return nullptr;
+        
+        vector<int> al;
+        order(al, root);
+        sort(al.begin(), al.end());
+        
+        idx = 0;
+        replace(al, root);
+        
+        return root;
+    }
+    
+    void order(vector<int>& a, Node* r) {
+        if (r == nullptr)
+            return;
+        else {
+            order(a, r->left);
+            a.push_back(r->data);
+            order(a, r->right);
+        }
+    }
+    
+    void replace(vector<int>& a, Node* root) {
+        if (root == nullptr)
+            return;
+        
+        replace(a, root->left);
+        
+        root->data = a[idx++];
+        
+        replace(a, root->right);
+    }
